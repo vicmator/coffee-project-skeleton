c9e5faeb79418c0b3d518961615410bc8a8e0fde
Santiago <santi.camargo.rodriguez@gmail.com>
2016-01-18 23:49:34 +0100

    Fixed clear-directories removing root .gitignore

--- clear-directories.sh
+++ clear-directories.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 cd `dirname $0`
-find . -type f -name ".*" ! -path "../.*" -delete && echo "Cleanup done"
+find . -type f -name ".*" ! -path "./.*" -delete && echo "Cleanup done"
 [[ -e clear-directories.sh ]] && rm clear-directories.sh
 [[ -e clear-directories.bat ]] && rm clear-directories.bat
-sed -i 's/^!\.gitgnore$//g' .gitignore
+sed -i 's#!\\\./\.\*/\.gitignore##g' .gitignore
